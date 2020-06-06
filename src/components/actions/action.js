ade34c12447b59c0fa8b821052d582c454d97b91
nichol88 <nichol88@gmail.com>
2020-06-06 12:07:18 -0400

    improve action list display

--- src/components/actions/action.js
+++ src/components/actions/action.js
@@ -4,15 +4,15 @@ import React from 'react';
 export default function Action(props) {
   // const [likes, setLikes] = useState(0)
 
-  const dateTime = props.action.created_at
-
+  const dateTime = new Date(props.action.created_at).toUTCString()
+  console.log(dateTime)
   // const handleOnClick = () => {
   //   setLikes(likes + 1)
   // }
 
   return (
     <li>
-      I did <strong>{props.action.reps} {props.action.name}</strong> on {dateTime.split('T')[0]} at {dateTime.split('T')[1].slice(0,5)} (UTC)
+      I did <strong>{props.action.reps} {props.action.name}</strong> on {dateTime}
     </li>
   )
 }
