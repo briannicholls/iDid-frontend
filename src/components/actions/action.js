17a5515d708e20cac01b1ba57a07f31411a5c64a
nichol88 <nichol88@gmail.com>
2020-06-06 12:08:31 -0400

    improve action list display

--- src/components/actions/action.js
+++ src/components/actions/action.js
@@ -1,14 +1,8 @@
 import React from 'react';
-// import {useState} from 'react';
 
 export default function Action(props) {
-  // const [likes, setLikes] = useState(0)
 
   const dateTime = new Date(props.action.created_at).toUTCString()
-  console.log(dateTime)
-  // const handleOnClick = () => {
-  //   setLikes(likes + 1)
-  // }
 
   return (
     <li>
