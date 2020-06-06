2e6c10af5dc0fc3805e0874ca0fe8c6dc33284a4
Brian Nicholls <nichol88@gmail.com>
2020-06-06 08:57:55 -0400

    fix mui theme component for app wide theme changes

--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,14 @@
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles'
 
-import red from '@material-ui/core/colors/red';
-import purple from '@material-ui/core/colors/purple';
+// import red from '@material-ui/core/colors/red';
+// import purple from '@material-ui/core/colors/purple';
 
 const theme = responsiveFontSizes(
   createMuiTheme({
-    // palette: {
-    //   primary: red,
-    //   secondary: purple,
-    // },
+    palette: {
+      // type: 'dark'
+    },
+
   })
 )
 
