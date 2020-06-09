ba5aaaf675d7c8a3a7eb456599f509f5473fe588
Brian Nicholls <nichol88@gmail.com>
2020-06-09 00:15:48 -0400

    adjust action display for time attribute

--- src/components/actions/action.js
+++ src/components/actions/action.js
@@ -2,15 +2,25 @@ import React from 'react';
 
 export default function Action(props) {
 
-  const dateTime = new Date(props.action.created_at).toUTCString()
+  // const dateTime = new Date(props.action.created_at).toUTCString()
+  const dateTime = props.action.created_at
 
   const weight = [props.action.weight, props.action.weight_unit]
+  const time = [props.action.time, props.action.time_unit]
   // console.log(props.action)
 
+  let weightInfo;
+  let timeInfo;
+
+  if (weight[0]) {
+    weightInfo = `(${weight[0]} ${weight[1]}s)`
+  } else if (time[0]) {
+    timeInfo = `(${time[0]} ${time[1]}s)`
+  }
 
   return (
     <li>
-      I did <strong>{props.action.reps} {props.action.name} {weight[0] ? `(${weight[0]} ${weight[1]}s)` : null}</strong> on {dateTime}
+      I did <strong>{props.action.reps} {props.action.counter.name} {timeInfo ? timeInfo : null} {weightInfo ? weightInfo : null}</strong> on {dateTime}
     </li>
   )
 }
