a894d3495ce9efc70ef02041a95dee0d90d25ddd
nichol88 <nichol88@gmail.com>
2020-06-27 01:33:53 -0400

    adjust Action display for new props

--- src/components/actions/action.js
+++ src/components/actions/action.js
@@ -2,25 +2,11 @@ import React from 'react';
 
 export default function Action(props) {
 
-  // const dateTime = new Date(props.action.created_at).toUTCString()
   const dateTime = props.action.when
-
-  const weight = [props.action.weight, props.action.weight_unit]
-  const time = [props.action.reps, props.action.time_unit]
-  // console.log(props.action)
-
-  let weightInfo;
-  let timeInfo;
-
-  if (weight[0]) {
-    weightInfo = `(${weight[0]} ${weight[1]}s)`
-  } else if (time[1]) {
-    timeInfo = `${time[0]} ${time[1]} of `
-  }
 
   return (
     <li>
-      I did <strong>{timeInfo ? timeInfo : props.action.reps} {props.action.counter.name}  {weightInfo ? weightInfo : null}</strong> on {dateTime}
+      I did <strong>{props.kind == "timed" ? `${props.action.reps} ${props.unit}s of` : props.action.reps} {props.action.counter.name}  {props.kind == "weighted" ? `(${props.action.weight} ${props.unit})` : null}</strong> on {dateTime}
     </li>
   )
 }
