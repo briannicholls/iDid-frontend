1efa3a763f270fc9ed03f9ed328c7dadab9b6ec7
Brian (Home) <nichol88@gmail.com>
2020-07-21 22:14:19 -0400

    attempt to fix cors bug

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,4 @@
 //export const BASE_URL = 'http://localhost:3001'
 // export const API = 'http://localhost:3001/api/v1'
 export const API = 'https://idid-app.herokuapp.com/api/v1'
-export const REQUEST_ORIGIN = 'https://idid.netlify.app'
-
+export const REQUEST_ORIGIN = 'https://idid.netlify.app/'
