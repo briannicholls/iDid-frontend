58518ec3b77c785a1b43591bbb32379780a77a81
Brian <nichol88@gmail.com>
2022-03-12 21:08:21 -0500

    clean up code

--- src/Constants.js
+++ src/Constants.js
@@ -1,8 +1,3 @@
-//export const BASE_URL = 'http://localhost:3001'
-// export const API_URL = 'http://localhost:3001/api/v1'
-// export const API_URL = 'https://idid-app.herokuapp.com/api/v1'
-// export const REQUEST_ORIGIN = 'https://idid.netlify.app/'
-
 export const API_URL = process.env.REACT_APP_API_URL
 export const REQUEST_ORIGIN = process.env.REACT_APP_REQUEST_ORIGIN
 
