f46b6b02e60b55a0aae9683220f5e8f834858b97
Brian <nichol88@gmail.com>
2022-03-12 23:51:50 -0500

    clean up code

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom' 
 import {Provider} from 'react-redux'
 import store from './store.js'
-import {MuiThemeProvider} from '@material-ui/core/styles'
-
-import theme from './theme'
 
 ReactDOM.render(
   <Provider store={store}>
--- src/theme.js
+++ src/theme.js
@@ -9,14 +9,21 @@ const theme = responsiveFontSizes(
       // type: 'dark',
       primary: {
         main: '#6f00a7',
-        light: '#a242d9',
-        dark: '#3b0077',
+        // light: '#a242d9',
+        // dark: '#3b0077',
       },
       secondary: {
         main: '#e040fb',
-        light: '#ff79ff',
-        dark: '#aa00c7',
-      }
+        // light: '#ff79ff',
+        // dark: '#aa00c7',
+      },
+      // Used by `getContrastText()` to maximize the contrast between
+      // the background and the text.
+      contrastThreshold: 3,
+      // Used by the functions below to shift a color's luminance by approximately
+      // two indexes within its tonal palette.
+      // E.g., shift from Red 500 to Red 300 or Red 700.
+      tonalOffset: 0.2,
     },
 
   })
