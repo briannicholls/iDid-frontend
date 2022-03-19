7d5a2a2423ca5d40ab7112523b5382fcf4d8adcb
Brian <nichol88@gmail.com>
2022-03-19 01:49:26 -0400

    fix loading bug on user show page

--- src/components/users/UserShow.js
+++ src/components/users/UserShow.js
@@ -3,12 +3,16 @@ import Typography from '@mui/material/Typography'
 import {connect} from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import {fetchUser} from '../../actions'
+import _ from 'lodash'
 
 const UserShow = ({ user, match, fetchUser }) => {
   useEffect(() => {
     fetchUser(match.params.id)
   }, [])
 
+  if (!user || _.isEmpty(user)) {
+    return <p>Loading...</p>
+  }
   
   return (
     <Typography variant="h1" color="initial">
